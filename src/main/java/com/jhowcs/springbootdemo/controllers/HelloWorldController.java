11a558e547b44d56f58d8aefd6cefd67ddb09753
Jonathan Campos <jhowcs@gmail.com>
2023-10-15 13:53:31 +0100

    * modify project name

--- src/main/java/com/jhowcs/springbootdemo/controllers/HelloWorldController.java
+++ src/main/java/com/jhowcs/springbootdemo/controllers/HelloWorldController.java
@@ -2,22 +2,16 @@ package com.jhowcs.springbootdemo.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/hello")
+@RequestMapping("/api/v1/fruits")
 public class HelloWorldController {
 
-    @GetMapping("{name}")
-    public ResponseEntity<String> hello(@PathVariable("name") String name) {
-        return ResponseEntity.ok("Hello " + name);
-    }
-
-    @GetMapping("fruits")
+    @GetMapping
     public ResponseEntity<List<String>> fruits() {
         var fruits = List.of("banana, apple, pineapple, peach, raspberry, blueberry, orange");
         return ResponseEntity.ok(fruits);
