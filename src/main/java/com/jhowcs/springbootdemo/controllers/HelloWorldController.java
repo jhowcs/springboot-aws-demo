d6abd504b0326c0be0cd3dd406904a01e25f9f11
Jonathan Campos <jhowcs@gmail.com>
2023-10-15 11:06:56 +0100

    * add static fruit list

--- src/main/java/com/jhowcs/springbootdemo/controllers/HelloWorldController.java
+++ src/main/java/com/jhowcs/springbootdemo/controllers/HelloWorldController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/hello")
 public class HelloWorldController {
@@ -13,5 +15,11 @@ public class HelloWorldController {
     @GetMapping("{name}")
     public ResponseEntity<String> hello(@PathVariable("name") String name) {
         return ResponseEntity.ok("Hello " + name);
+    }
+
+    @GetMapping("fruits")
+    public ResponseEntity<List<String>> fruits() {
+        var fruits = List.of("banana, apple, pineapple, peach, raspberry, blueberry");
+        return ResponseEntity.ok(fruits);
     }
 }
